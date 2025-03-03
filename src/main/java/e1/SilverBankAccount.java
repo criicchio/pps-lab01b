435f1fc0aadf2d5b91c9d4de634940a796150ad6
Cristina Zoccola <cristina.zoccola@studio.unibo.it>
2025-03-03 10:59:33 +0100

    add interface changes

--- src/main/java/e1/SilverBankAccount.java
+++ src/main/java/e1/SilverBankAccount.java
@@ -4,14 +4,17 @@ public class SilverBankAccount implements BankAccount{
 
     private CoreBankAccount base = new CoreBankAccount();
 
+    @Override
     public int getBalance() {
         return base.getBalance();
     }
 
+    @Override
     public void deposit(int amount) {
         base.deposit(amount);
     }
 
+    @Override
     public void withdraw(int amount) {
         if (this.getBalance() < amount){
             throw new IllegalStateException();
